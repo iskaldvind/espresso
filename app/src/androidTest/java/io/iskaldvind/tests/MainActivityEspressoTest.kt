0a7a2eba524bcf396d469a53d0e6002b78234376
iskaldvind <iskaldvind@gmail.com>
2021-12-16 16:56:45 +0300

    hw

--- app/src/androidTest/java/io/iskaldvind/tests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/io/iskaldvind/tests/MainActivityEspressoTest.kt
@@ -1,8 +1,10 @@
 package io.iskaldvind.tests
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
@@ -10,6 +12,7 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import io.iskaldvind.tests.view.search.MainActivity
 import junit.framework.TestCase
 import org.hamcrest.Matcher
@@ -32,15 +35,15 @@ class MainActivityEspressoTest {
     fun activitySearch_IsWorking() {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
-        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+        onView(withId(R.id.searchButton)).perform(click())
 
         onView(isRoot()).perform(delay())
-        onView(withId(R.id.totalCountTextView)).check(matches(withText(R.string.test_value)))
+        onView(withId(R.id.totalCountTextView)).check(matches(withText(InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.test_value_espresso))))
         onView(withId(R.id.totalCountTextView)).check(matches(isDisplayed()))
         onView(withId(R.id.totalCountTextView)).check(matches(isCompletelyDisplayed()))
     }
 
-    private fun delay(): ViewAction? {
+    private fun delay(): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
             override fun getDescription(): String = "wait for $2 seconds"
--- app/src/main/java/io/iskaldvind/tests/view/search/MainActivity.kt
+++ app/src/main/java/io/iskaldvind/tests/view/search/MainActivity.kt
@@ -2,8 +2,6 @@ package io.iskaldvind.tests.view.search
 
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.iskaldvind.tests.R
@@ -45,23 +43,18 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
     }
 
     private fun setQueryListener() {
-        searchEditText.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = searchEditText.text.toString()
-                if (query.isNotBlank()) {
-                    presenter.searchGitHub(query)
-                    return@OnEditorActionListener true
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        getString(R.string.enter_search_word),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@OnEditorActionListener false
-                }
+        searchButton.setOnClickListener {
+            val query = searchEditText.text.toString()
+            if (query.isNotBlank()) {
+                presenter.searchGitHub(query)
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.enter_search_word),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-            false
-        })
+        }
     }
 
     private fun createRepository(): RepositoryContract =
@@ -106,6 +99,5 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     companion object {
         const val BASE_URL = "https://api.github.com"
-        const val FAKE = "FAKE"
     }
 }
